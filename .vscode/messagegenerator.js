99fd7f3cd62044a366f6ca660110f135a7a76f4d
Maria Simeon-Godfrey <m4mariaoni@yahoo.com>
2024-02-28 20:18:39 +0000

    msgenerator

--- .vscode/messagegenerator.js
+++ .vscode/messagegenerator.js
@@ -1,1 +1,15 @@
-const message = ['wake up', 'check the phone', 'pray', 'go to bathroom', 'eat', 'setup for the day'];
+
+
+const lifeStyle = {
+    firstSet : ['wake up', 'check the phone', 'pray', 'go to bathroom'],
+    secondSet : ['eat','check messages'],
+    thirdSet : ['setup for the day', 'do daily task', 'reading & personal development']
+}
+
+const generateRandom = num => Math.floor(Math.random() * num);
+
+for (let style in lifeStyle){
+    let index = generateRandom(lifeStyle[style].length)
+    console.log(style);
+    console.log(index);
+}
